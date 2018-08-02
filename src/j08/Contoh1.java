190ec43695f290cec2464c2e54e36fb218d079a1
Celvin Ari <Celvin Ari@Celvin>
2018-08-02 11:14:05 +0700

    FOR WHILE DO WHILE

---
+++ src/j08/Contoh1.java
@@ -0,0 +1,27 @@
+
+package j08;
+import java.util.Scanner;
+public class Contoh1 {
+    public static void main(String [] args){
+        int x;
+        for(x = 1; x <= 5; x++)
+        {
+            System.out.println("SMK Telkom Malang");
+        }
+            
+        int i = 1;
+        while(i <= 5)
+        {
+            System.out.println("SMK Telkom Malang");
+            i++;
+        }
+        
+        int j = 1;
+        do
+        {
+            System.out.println("SMK Telkom Malang");
+            j++;
+        }       
+        while(j <= 5);
+    }
+}
